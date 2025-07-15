ab0dfa46f79a2ef31267a5893083de5ff279939a
Chan-Hyung <homework201@naver.com>
2025-07-15 19:47:06 +0900

    Create 06_DDL(CREATE)_DDL계정.sql

---
+++ 02_DDL(CREATE)/06_DDL(CREATE)_DDL계정.sql
@@ -0,0 +1,608 @@
+/*
+    - DDL (DATA DEFINITION LANGUAGE) : 데이터 정의 언어
+    오라클에서 제공하는 객체(OBJECT)를 새로이 만들고(CREATE), 구조를 변경하고(ALTER), 구조를 삭제하는(DROP) 언어
+    즉, 실제데이터가 값이 아닌 구조 자체를 정의하는 언어
+    주로 DB관리자, 설계자가 사용
+    
+    오라클에서 제공하는 객체(OBJECT) : 테이블(TABLE), 뷰(VIEW), 시퀀스(SEQUENCE), 인덱스(INDEX), 패키지(PACKAGE), 트리거(TRIGGER), 프로시져(PROCEDURE), 함수(FUNCTION), 사용자(USER)
+    
+    <CREATE>
+    객체를 새로이 생성하는 구문
+    
+*/
+/*
+    1. 테이블 생성
+    - 테이블이란? 행(ROW)과 열(COLUMN(로 구성되는 가장 기본적인 데이터베이스 객체
+                모든 데이터들은 테이블을 통해서 저장함.
+                (DBMS 용어 중 하나로, 데이터를 일종의 표 형태로 표현한 것)
+                
+    (표현식)
+    CREATE TABLE 테이블명(
+        컬럼명 자료형(크기),
+        컬럼명 자료형(크기),
+        컬럼명 자료형,
+        ...
+    );
+    
+    * 자료형
+    - 문자(CHAR(바이트크기)) | VARCHAR2(바이트크기) => 반드시 크기 지정
+    > CHAR : 최대 2000바이트 까지 지정. 지정한 범위 안에서만 써야함 / 고정길이 (지정한 크기보다 더 적은값이 들어와도 공백으로 채워줘야함)
+            고정된 글자수의 데이터만이 담길 경우 사용
+            
+    > VARCHAR2 : 최대 4000바이트까지 지정 가능, 가변길이( 담긴 값에 따라서 공간의 크기 맞춰짐/ 단, 크기가 지정한 값보다 크면 오류남)
+                몇글자의 데이터가 들어올지 모를 경우 사용
+                
+    - 숫자 (NUMBER)
+    
+    - 날짜 (DATE)
+    
+*/
+
+--------> 회원에 대한 데이터를 담기 위한 테이블 MEBER생성
+CREATE TABLE MEMBER (
+    MEM_NO NUMBER PRIMARY KEY,        -- 회원번호, 기본키
+    MEM_ID VARCHAR2(20) NOT NULL,     -- 아이디
+    MEM_PWD VARCHAR2(20) NOT NULL,    -- 비밀번호
+    MEM_NAME VARCHAR2(20) NOT NULL,   -- 이름
+    GENDER CHAR(1),                   -- 성별 (M/F)
+    PHONE VARCHAR2(13),               -- 전화번호
+    EMAIL VARCHAR2(50),               -- 이메일
+    MEM_DATE DATE DEFAULT SYSDATE     -- 가입일, 기본값 오늘
+);
+
+-- 만약 컬럼명에 오타가 발생했다면? 다시만들면 안되고 삭제하고 다시 만들어야함
+
+---------------------------------------------------------------------------------
+/*
+    2. 컬럼에 주석 달기(컬럼에 대한 설명 같은거)
+    
+    (표현법)
+    COMMENT ON COLUMN 테이블명.컬럼명 IS '주석내용';
+*/
+
+--COMMENT ON COLUMN MEMBER.MEM_NO IS 'ㅁㄴㅇㄹ'; 오타나도 그냥 다시 작성하여 고칠 수 있음
+COMMENT ON COLUMN MEMBER.MEM_NO IS '회원번호';
+
+COMMENT ON COLUMN MEMBER.MEM_ID IS '회원아이디';
+COMMENT ON COLUMN MEMBER.MEM_PWD IS '회원비밀번호';
+COMMENT ON COLUMN MEMBER.MEM_NAME IS '회원명';
+COMMENT ON COLUMN MEMBER.GENDER IS '성별(남/여)';
+COMMENT ON COLUMN MEMBER.PHONE IS '전화번호';
+COMMENT ON COLUMN MEMBER.EMAIL IS '이메일';
+COMMENT ON COLUMN MEMBER.MEM_DATE IS '회원가입일';
+--------------------------------------------------------------------------------------------
+-- 테이블에 데이터를 추가 시키는 구문(DML : INSERT) 이때 자세히하게 배움
+-- INSERT INTO 테이블명 VALUES(값1, 값2, ...);
+SELECT * FROM MEMBER;
+INSERT INTO MEMBER VALUES(1, 'user01', 'pass01', '차은우', '1', '010-1111-2222', 'ASDF@NAVER.COM', '24/12/24');
+INSERT INTO MEMBER VALUES(2, 'user02', 'pass02', '장원영', '2', null, null, SYSDATE);
+INSERT INTO MEMBER VALUES(NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+-- 유효하지 않은 데이터가 들어가고 있음.. 뭔가 조건을 걸어줘야함.
+
+-----------------------------------------------------------------------------------------------------------------------
+/*
+    <제약조건 CONSTRAINT>
+    - 원하는 데이터 값만 유지하기 위해서 특정 컬럼에 설정하는 제약소건
+    - 데이터 무결성 보장을 목적으로 한다.
+    
+    * 종류 : NOT NULL, UNIQUE, CHECK, PRIMARY KEY, FOREIGN KEY
+*/
+/*
+    * NOT NULL 제약조건
+    해당 컬럼에 반드시 값이 존재해야만 할 경우 (즉, 해당 컬럼에 절대NULL)이 들어와서는 안됨
+    삽입 / 수정시 NULL 값을 허용하지 않도록 제한
+    
+    제약 조건을 부여하는 방식은 크게 2가지 방식이 있음(컬럼레벨방식/테이블레벨방식)
+    * NOT  NULL 제약조건은 오로지 컬럼 레벨 방식 밖에 안됨.
+*/
+
+-- 컬럼레벨방식 : 컬럼명 자료형 제약조건
+
+CREATE TABLE MEM_NOTNULL (
+    MEM_NO NUMBER NOT NULL,        -- 회원번호, 기본키
+    MEM_ID VARCHAR2(20) NOT NULL,     -- 아이디
+    MEM_PWD VARCHAR2(20) NOT NULL,    -- 비밀번호
+    MEM_NAME VARCHAR2(20) NOT NULL,   -- 이름
+    GENDER CHAR(3),                   -- 성별 (M/F)
+    PHONE VARCHAR2(13),               -- 전화번호
+    EMAIL VARCHAR2(50),               -- 이메일
+    MEM_DATE DATE DEFAULT SYSDATE     -- 가입일, 기본값 오늘
+);
+
+SELECT * FROM MEM_NOTNULL;
+
+INSERT INTO MEM_NOTNULL VALUES(1, 'user01', 'pass01' , '차은우', '남', '010-1111-2222', 'asdf@naver.com', sysdate );
+INSERT INTO MEM_NOTNULL VALUES(2, 'user02', null , '박보검', '남', null, 'qwer@naver.com', null);
+-- ORA-01400: cannot insert NULL into ("DDL"."MEM_NOTNULL"."MEM_PWD")
+-- 의도 했던 대로 오류(not null 제약조건에 위배되어 오류 발생)
+INSERT INTO MEM_NOTNULL VALUES(2, 'user01', 'pass02' , '차은우', '남', null, null, null );
+-- 아이디가 중복되어있음에도 불구하고 자 추가 됨;
+----------------------------------------------------------------------------------------------------------------------
+/*
+    *UNIQUE 제약 조건
+    해당 컬럼에 중복된 값이 들어가서는 안 될 경우
+    컬럼값에 중복값을 제한하는 제약조건
+    삽입 / 수정시 기존에 있는 데이터값 중 중복값이 
+*/
+
+CREATE TABLE MEM_UNIQUE (
+    MEM_NO NUMBER NOT NULL,    
+    MEM_ID VARCHAR2(20) NOT NULL UNIQUE, -- 컬럼 레벨 방식  
+    MEM_PWD VARCHAR2(20) NOT NULL,   
+    MEM_NAME VARCHAR2(20) NOT NULL, 
+    GENDER CHAR(3),                
+    PHONE VARCHAR2(13),              
+    EMAIL VARCHAR2(50)              
+);
+
+SELECT * FROM MEM_UNIQUE;
+DROP TABLE MEM_UNIQUE;
+
+-- 테이블레벨 방식 : 모든 컬럼들 다 나열 한 후 마지막에 기술하는 방식
+--                 제약 조건(컬럼명)
+
+CREATE TABLE MEM_UNIQUE (
+    MEM_NO NUMBER NOT NULL,    
+    MEM_ID VARCHAR2(20) NOT NULL,
+    MEM_PWD VARCHAR2(20) NOT NULL,   
+    MEM_NAME VARCHAR2(20) NOT NULL, 
+    GENDER CHAR(3),                
+    PHONE VARCHAR2(13),              
+    EMAIL VARCHAR2(50),
+    UNIQUE(MEM_ID) -- 테이블 레벨 방식
+);
+
+SELECT * FROM MEM_UNIQUE;
+
+INSERT INTO MEM_UNIQUE VALUES(1, 'user01', 'pass01', '차은우', null, null, null);
+INSERT INTO MEM_UNIQUE VALUES(2, 'user01', 'pass02', '박보검', null, null, null);
+-- ORA-00001: unique constraint (DDL.SYS_C007056) violated
+-- unique 제약조건에 위배되어 오류발생
+-- > 오류 구문을 제약조건명으로 알려줌(특정 컬럼에 어떤 문제가 잇는지 상세히 알려주지 않음)
+-- > 쉽게 파악하기 어려움
+-- > 제약조건 부여시 제약조건명을 지정해주지 않으면 시스템에서 임의의 제약조건명을 부여해 버림.
+
+/*
+    * 제약조건 부여시 제약조건명까지 지어주는 방법
+    
+    1. 컬럼레벨 방식
+    CREATE TABLE 테이블명(
+        컬럼명 자료형 [CONSTRAINT 제약조건명],
+        컬럼명 자료형
+    );
+    
+    2. 테이블레벨 방식
+    CREATE TABLE 테이블명(
+        컬럼명 자료형,
+        컬럼명 자료형,
+        [CONSTRAINT 제약조건명] 제약조건(컬럼명)
+    );
+*/
+
+DROP TABLE MEM_UNIQUE;
+
+CREATE TABLE MEM_NOTNULL (
+    MEM_NO NUMBER CONSTRAINT MEMNO_NN NOT NULL,       
+    MEM_ID VARCHAR2(20) CONSTRAINT MEMID_NN NOT NULL,   
+    MEM_PWD VARCHAR2(20) CONSTRAINT MEMPWD_NN NOT NULL,  
+    MEM_NAME VARCHAR2(20) CONSTRAINT MEMNAME_NN NOT NULL,  
+    GENDER CHAR(3),                   
+    PHONE VARCHAR2(13),               
+    EMAIL VARCHAR2(50),               
+    CONSTRAINT MEMID_UQ UNIQUE(MEM_ID)
+);
+
+SELECT * FROM MEM_NOTNULL;
+
+INSERT INTO MEM_NOTNULL VALUES(1, 'user01', 'pass01' , '차은우', NULL, NULL, NULL );
+INSERT INTO MEM_NOTNULL VALUES(2, 'user02', 'pass02' , '박보검', NULL, NULL, NULL);
+INSERT INTO MEM_NOTNULL VALUES(3, 'user03', 'pass03' , '장원영', 'ㄴ', NULL, NULL);
+-- 성병에 유효한 값이 아닌게 들어와도 잘 insert 됨
+----------------------------------------------------------------------------------------------
+/*
+    CHECK 제약 조건
+    해당 컬럼에 들어올 수 잇는 값에 대한 조건을 제시
+    해당 조건에 만족하는 데이터값만 담길 수 있음
+*/
+
+CREATE TABLE MEM_CHECK (
+    MEM_NO NUMBER NOT NULL,    
+    MEM_ID VARCHAR2(20) NOT NULL UNIQUE,  
+    MEM_PWD VARCHAR2(20) NOT NULL,   
+    MEM_NAME VARCHAR2(20) NOT NULL, 
+    GENDER CHAR(3) CHECK(GENDER IN ('남', '여')),    -- 컬럼레벨방식
+--  CHECK(GENDER IN ('남','여')) -- 테이블레벨방식
+    PHONE VARCHAR2(13),              
+    EMAIL VARCHAR2(50)              
+);
+
+SELECT * FROM MEM_CHECK;
+
+INSERT INTO MEM_CHECK
+VALUES(1, 'user01', 'pass01', '차은우', '남', null, null);
+
+INSERT INTO MEM_CHECK
+VALUES(2, 'user02', 'pass02', '박보검', 'ㄴ', null, null);
+-- ORA-02290: check constraint (DDL.SYS_C007061) violated
+-- check 제약조건에 위배됐기 때문에 오류 발생
+
+INSERT INTO MEM_CHECK
+VALUES(2, 'user02', 'pass02', '박보검', null, null, null);
+-- 만약, 성별컬럼에 데이터 값을 넣고자 한다면 check 제약조건에 만족하는 값을 넣어야하지만,
+-- not null 아니면 null도 가능하긴하다.
+
+INSERT INTO MEM_CHECK
+VALUES(2, 'user03', 'pass03', '장원영', null, null, null);
+-- 회원번호가 동일해도 오류가 발생하지가 않음
+
+------------------------------------------------------------------------------------------
+/*
+    * PRIMARY KEY(기본키) 제약조건
+    테이블에서 각 행들을 식별하기 위해 사용될 컬럼에 부여하는 제약조건(식별자의 역할)
+    
+    EX) 회원번호, 학번, 사원번호, 부서코드, 직급코드, 주문번호
+    
+    PRIMARY KEY 제약조건을 부여하면 그 컬럼에 자동으로 NOT NULL + UNIQUE 제약조건을 가짐
+    단, 한테이블당 한개만 설정 가능
+*/
+CREATE TABLE MEM_PRI (
+    MEM_NO NUMBER CONSTRAINT MEMNO_PK PRIMARY KEY, -- 컬럼 레벨 방식   
+    MEM_ID VARCHAR2(20) NOT NULL UNIQUE,  
+    MEM_PWD VARCHAR2(20) NOT NULL,   
+    MEM_NAME VARCHAR2(20) NOT NULL, 
+    GENDER CHAR(3) CHECK(GENDER IN ('남', '여')),   
+    PHONE VARCHAR2(13),              
+    EMAIL VARCHAR2(50)              
+    -- CONSTRAINT MEMNO_PK PRIMARY KEY(MEM_NO) -- 테이블 레벨 방식
+);
+
+SELECT * FROM MEM_PRI;
+
+INSERT INTO MEM_PRI
+VALUES(1, 'user01', 'pass01', '차은우', '남', '010-1111-2222', null);
+
+INSERT INTO MEM_PRI
+VALUES(1, 'user02', 'pass02', '박보검', '남', null, null);
+-- ORA-00001: unique constraint (DDL.MEMNO_PK) violated
+-- 기본키에 중복값을 담으려고 할 때 unique 제약조건에 위배
+
+INSERT INTO MEM_PRI
+VALUES(null, 'user02', 'pass02', '박보검', '남', null, null);
+-- ORA-01400: cannot insert NULL into ("DDL"."MEM_PRI"."MEM_NO")
+-- 기본키에 null을 담으려고 할때 (not null 제약조건에 위배)
+
+INSERT INTO MEM_PRI
+VALUES(2, 'user02', 'pass02', '박보검', '남', null, null);
+
+CREATE TABLE MEM_PRI2 (
+    MEM_NO NUMBER CONSTRAINT MEMNO_PK PRIMARY KEY,
+    MEM_ID VARCHAR2(20) PRIMARY KEY,  
+    MEM_PWD VARCHAR2(20) NOT NULL,   
+    MEM_NAME VARCHAR2(20) NOT NULL, 
+    GENDER CHAR(3) CHECK(GENDER IN ('남', '여')),   
+    PHONE VARCHAR2(13),              
+    EMAIL VARCHAR2(50)
+);
+
+-- ORA-02260: table can have only one primary key
+-- 기본키는 하나만 됨.
+
+CREATE TABLE MEM_PRI2 (
+    MEM_NO NUMBER ,
+    MEM_ID VARCHAR2(20) ,  
+    MEM_PWD VARCHAR2(20) NOT NULL,   
+    MEM_NAME VARCHAR2(20) NOT NULL, 
+    GENDER CHAR(3) CHECK(GENDER IN ('남', '여')),   
+    PHONE VARCHAR2(13),              
+    EMAIL VARCHAR2(50),
+    primary key(MEM_NO, MEM_ID) -- 묶어서 PRIMARY KEY 제약조건 부여(복합키)
+);
+
+SELECT * FROM MEM_PRI2;
+
+INSERT INTO MEM_PRI2
+VALUES(1, 'user01', 'pass01', '차은우', null, null, null);
+
+INSERT INTO MEM_PRI2
+VALUES(1, 'user02', 'pass02', '박보검', null, null, null);
+
+INSERT INTO MEM_PRI2
+VALUES(2, 'user02', 'pass02', '장원영', null, null, null);
+
+INSERT INTO MEM_PRI2
+VALUES(null, 'user02', 'pass02', '박정민', null, null, null);
+-- ORA-01400: cannot insert NULL into ("DDL"."MEM_PRI2"."MEM_NO")
+-- PRIMARY KEY로 묶여있는 각 컬럼에는 절대 NULL을 허용하지 않음
+
+-- 복합키 사용 예시(찝하기, 좋아요, 구독)
+-- 찝하기 : 한상품은 오로지 한번만 찜할 수 있음
+-- 어떤 회원이 어떤 상품을 찜하는지에 대한 데이터를 보관하는 테이블
+
+CREATE TABLE TB_LIKE(
+    MEM_NO NUMBER,
+    PRODUCT_NAME VARCHAR2(40),
+    LIKEDATE DATE,
+    PRIMARY KEY( MEM_NO, PRODUCT_NAME)
+);
+
+SELECT * FROM TB_LIKE;
+INSERT INTO TB_TABLE VALUES(1,'백도 말랑이 ', SYSDATE);
+INSERT INTO TB_TABLE VALUES(1,'백도 말랑이 ', SYSDATE);
+INSERT INTO TB_TABLE VALUES(2,'백도 말랑이 ', SYSDATE);
+
+--------------------------------------------------------------------
+-- 회원 등급에 대한 데이터를 따로 보고나하는 테이블
+CREATE TABLE MEM_GRADE(
+    GRADE_CODE NUMBER PRIMARY KEY,
+    GRADE_NAME VARCHAR2(30) NOT NULL
+);
+
+SELECT * FROM MEM_GRADE;
+
+INSERT INTO MEM_GRADE VALUES(10, '일반회원');
+INSERT INTO MEM_GRADE VALUES(20, '우수회원');
+INSERT INTO MEM_GRADE VALUES(30, '특별회원');
+
+CREATE TABLE MEM (
+    MEM_NO NUMBER PRIMARY KEY,    
+    MEM_ID VARCHAR2(20) NOT NULL UNIQUE,  
+    MEM_PWD VARCHAR2(20) NOT NULL,   
+    MEM_NAME VARCHAR2(20) NOT NULL, 
+    GENDER CHAR(3) CHECK(GENDER IN ('남', '여')),   
+    PHONE VARCHAR2(13),              
+    EMAIL VARCHAR2(50),
+    GRADE_ID NUMBER --학원 등급번호 같이 보관할 컬럼
+);
+
+SELECT * FROM MEM;
+
+INSERT INTO MEM
+VALUES(1, 'user01', 'pass01', '차은우', '남', null, null, null);
+
+INSERT INTO MEM
+VALUES(2, 'user02', 'pass02=', '박보검', '남', null, null, 10);
+
+INSERT INTO MEM
+VALUES(3, 'user03', 'pass03=' , '장원영', '남', null, null, 30);
+----------------------------------------------------------------------
+/*
+  * FOREIGN KEY(외래키) 제약 조건
+  - 다른 테이블에 존재하는 값만 들어와야 되는 특정 컬럼에 부여하는 제약조건
+  - 다른 테이블에 참조한다는 표현
+  - 주로 FK 제약조건에 의해 테이블간의 관계가 형성
+  
+  1.컬럼레벨방식
+  컬럼명 자료형 CONSTRAINT 제약조건명 REFERNECES 참조할 테이블명(참조할 컬럼명)
+  
+  2. 테이블레벨방식
+  CONSTRAINT 제약조건명 FOREIGN KEY(컬럼명) REFERENCES 참조할 테이명(참조할 컬럼명)
+  
+  --> 팜조할 컬럼명 생략시 참조할 테이블의 PK로 자동 설정
+  
+*/
+CREATE TABLE MEM (
+    MEM_NO NUMBER PRIMARY KEY,    
+    MEM_ID VARCHAR2(20) NOT NULL UNIQUE,  
+    MEM_PWD VARCHAR2(20) NOT NULL,   
+    MEM_NAME VARCHAR2(20) NOT NULL, 
+    GENDER CHAR(3) CHECK(GENDER IN ('남', '여')),   
+    PHONE VARCHAR2(13),              
+    EMAIL VARCHAR2(50),
+    GRADE_ID NUMBER REFERENCES MEM_GRADE(GRADE_CODE) -- 컬럼 레벨 방식 : 참조컬럼명 생각하면 PK랑 자동 연결
+    -- FOREIGN KEY(GRADE_ID) REFERENCES MEM_GRADE(GRADE_CODE) -- 테이블 레벨 방식
+);
+
+SELECT * FROM MEM;
+
+INSERT INTO MEM
+VALUES(1, 'user01', 'pass01', '차은우', '남', null, null, null);
+
+INSERT INTO MEM
+VALUES(2, 'user02', 'pass02=', '박보검', '남', null, null, 10);
+
+INSERT INTO MEM
+VALUES(3, 'user03', 'pass03=' , '장원영', '남', null, null, 90); -- 30이면가능
+-- ORA-00001: unique constraint (DDL.SYS_C007080) violated
+-- PARENT KEY를 찾을 수 없다는 오류 발생
+-- MEM_GRADE(부모테이블) ------------ MEM(자식테이블)
+
+-- 이때 부모테이블에서 데이터값을 삭제할 경우
+-- 데이터 삭제 : DELETE FROM 테이블명 WHERE 조건;
+
+-- MEM_GRADE 테이블 10번 등급 삭제;
+DELETE FROM MEM_GRADE
+WHERE GRADE_CODE = 10;
+--> 자식테이블(MEM)에 10이라는 값을 사용하기 때문에 삭제X
+
+DELETE FROM MEM_GRADE
+WHERE GRADE_CODE = 20;
+--> 자식 테이블(MEM)에 20이라는 값을 사용하고 있지 않기 때문에 삭제 O
+
+--> 자식테이블에 이미 사용하고 있는 값이 있을 겨우
+--> 부모테이블로 부터 무조건 삭제가 안되게 하는 "삭제제한" 옵션이 걸려 있음
+
+SELECT * FROM MEM_GRADE;
+
+ROLLBACK;
+
+-------------------------------------------------------------------------------
+/*
+    자식 테이블 생성시 외래키 제약조건 부여할 때 삭제 옵션 지정 가능
+    * 삭제 옵션 : 부모테이블의 데이터 삭제시 그 데이터를 사용하고 있는 자식테이블 값을 처리 방법
+    
+    1. ON DELETE RESTRICTED(기본값) : 삭제 제한 옵션, 자식데이터로 쓰이는 부모데이터는 삭제 X
+    2. ON DELETE SET NULL : 부모데이터 삭제시 해당 데이터를 쓰고 있는 자식데이터의 값을 NULL로 변경
+    3. ON DELETE CASCADE : 부모데이터 삭제시 해당 데이터를 쓰고 있는 자식데이터도 같이 삭제
+*/
+
+DROP TABLE MEM;
+
+-- ON DELETE SET NULL
+
+CREATE TABLE MEM (
+    MEM_NO NUMBER PRIMARY KEY,    
+    MEM_ID VARCHAR2(20) NOT NULL UNIQUE,  
+    MEM_PWD VARCHAR2(20) NOT NULL,   
+    MEM_NAME VARCHAR2(20) NOT NULL, 
+    GENDER CHAR(3) CHECK(GENDER IN ('남', '여')),   
+    PHONE VARCHAR2(13),              
+    EMAIL VARCHAR2(50),
+    GRADE_ID NUMBER REFERENCES MEM_GRADE(GRADE_CODE) ON DELETE SET NULL
+);
+
+SELECT * FROM MEM;
+
+INSERT INTO MEM
+VALUES(1, 'user01', 'pass01', '차은우', '남', null, null, null);
+
+INSERT INTO MEM
+VALUES(2, 'user02', 'pass02', '박보검', '남', null, null, 10);
+
+INSERT INTO MEM
+VALUES(3, 'user03', 'pass03', '장원영', '여', null, null, 20);
+
+INSERT INTO MEM
+VALUES(4, 'user04', 'pass04', '박보영', '여', null, null, 10);
+
+-- 10번 등급 삭제
+DELETE FROM MEM_GRADE
+WHERE GRADE_CODE = 10;
+-- 10을 가져다 쓰고 있던 자식데이터 값은 NULL로 변경
+
+SELECT * FROM MEM_GRADE;
+SELECT * FROM MEM;
+
+ROLLBACK;
+
+DROP TABLE MEM;
+
+-- ON DELETE CASCADE
+
+CREATE TABLE MEM (
+    MEM_NO NUMBER PRIMARY KEY,    
+    MEM_ID VARCHAR2(20) NOT NULL UNIQUE,  
+    MEM_PWD VARCHAR2(20) NOT NULL,   
+    MEM_NAME VARCHAR2(20) NOT NULL, 
+    GENDER CHAR(3) CHECK(GENDER IN ('남', '여')),   
+    PHONE VARCHAR2(13),              
+    EMAIL VARCHAR2(50),
+    GRADE_ID NUMBER REFERENCES MEM_GRADE(GRADE_CODE) ON DELETE CASCADE
+);
+
+SELECT * FROM MEM;
+
+INSERT INTO MEM
+VALUES(1, 'user01', 'pass01', '차은우', '남', null, null, null);
+
+INSERT INTO MEM
+VALUES(2, 'user02', 'pass02', '박보검', '남', null, null, 10);
+
+INSERT INTO MEM
+VALUES(3, 'user03', 'pass03', '장원영', '여', null, null, 20);
+
+INSERT INTO MEM
+VALUES(4, 'user04', 'pass04', '박보영', '여', null, null, 10);
+
+-- 10등급 삭제
+DELETE FROM MEM_GRADE
+WHERE GRADE_CODE = 10;
+
+SELECT * FROM MEM_GRADE;
+SELECT * FROM MEM;
+
+----------------------------------------------------------------
+/*
+    <DEFAULT 기본값>
+    컴럼을 선정하지 않고 INSERT시 NULL이 아닌 기본ㄱ밧을 INSERT하고자 할 때 셋팅해둘 수 있는 값
+*/
+
+DROP TABLE MEMBER;
+-- 컬럼명 자료형 DEFAULT 기본값(제약조건)
+CREATE TABLE MEMBER(
+    MEM_NO NUMBER PRIMARY KEY,
+    MEM_NAME VARCHAR2(20) NOT NULL,
+    MEM_AGE NUMBER,
+    HOBBY VARCHAR2(20) DEFAULT '없음',
+    ENROLL_DATE DATE DEFAULT SYSDATE
+);
+
+SELECT * FROM MEMBER;
+
+INSERT INTO MEMBER VALUES(1, '차은우', 20, '노래하기', '25/01/01');
+INSERT INTO MEMBER VALUES(2, '박보검', NULL, NULL, NULL);
+INSERT INTO MEMBER VALUES(2, '박보검', NULL, DEFAULT, DEFAULT);
+
+-- INSERT INTO 테이블명(컬러명, 컬럼명) VALUES(값1, 값2);
+-- NOT NULL인건 꼭 골라야함.
+INSERT INTO MEMBER(MEM_NO, MEM_NAME) VALUES(4, '박정민');
+-- 선택되지 않은 컬럼에는 기본적으로 NULL이 기고
+--단, 해당 컬럼에 DEFAULT 값이 있을 경우 NULL이 아닌 DEFAULT 값이 들어감
+-------------------------------------------------------------------------------------------
+/*
+    ======================== KH 계정 ========================================
+    <SUBQUERY를 이용한 테이블 생성>
+    테이블 
+    
+    (표현식)
+    CREATE TABLE 테이블명
+    AS 서브쿼리;
+*/
+
+-- EMPLOYEE 테이블을 복제한 새로운 테이블 생성
+CREATE TABLE EMPLOYEE_COPY
+AS SELECT *
+    FROM EMPLOYEE;
+-- 컬럼, 테이터값, 제약조건 같은 경우에는 NOT NULL만 복사됨
+
+SELECT * FROM EMPLOYEE_COPY;
+
+CREATE TABLE EMPLOYEE_COPY2
+AS SELECT *
+    FROM EMPLOYEE -- 테이블의 구조만 가져오고싷다
+    WHERE 1 = 0; -- 무조건 FALSE인 조건 : 구조만을 복사하고자 할 때 쓰이는 구문(데이터 값은 필요없을때)
+    
+SELECT * FROM EMPLOYEE_COPY2;
+
+CREATE TABLE EMPLOYEE_COPY3
+AS SELECT EMP_ID, EMP_NAME, SALARY, SALARY * 12 AS "연봉"
+FROM EMPLOYEE;
+-- ORA-00998: must name this expression with a column alias
+-- ALIAS : 별칭
+----> 서브쿼리 SELECT 집에 산술식, 함수 같은데 기술된 경우 반드시 별칭 지정
+
+SELECT EMP_NAME, 연봉 FROM EMPLOYEE_COPY3;
+
+--------------------------------------------------------------------------
+/*
+    테이블 다 생성된 후에 뒤늦게 제약조건 추가
+    
+    ALTER TABLE 테이블명 변경할 내용;
+    
+    1) PRIMARY KEY : ALTER TABLE 테이블명 ADD PRIMARY KEY(컬럼명);
+    2) FOREIGN KEY :  ALTER TABLE 테이블명 ADD FOREIGN KEY(컬럼명) REFERENCES 참조할 테이블명(참조할 컬럼명)
+    3) UNIQUE :  ALTER TABLE 테이블명 ADD 테이블명 ADD UNIQUE(컬럼명) : 
+    4) CHECK :  ALTER TABLE 테이블명 ADD CHECK(컬럼에 대한 조건식)
+    5) NOT NULL : ALTER TABLE 테이블명 MODIFY 컬럼명 NOT NULL;
+*/
+
+-- 서브쿼리를 이용해서 복제한 테이블은 NN 제약조건 빼고 복제안됨
+-- EMPLOYEE_COPY 테이블 PK제약조건 추가(EMP_ID)
+
+ALTER TABLE EMPLOYEE_COPY ADD PRIMARY KEY(EMP_ID);
+
+-- EMPLOYEE 테이블에 DEPT_CODE에 외래키 제약조건 추가 (참조테이블(부모) : DEPARTMENT(DEPT_ID))
+ALTER TABLE EMPLOYEE ADD FOREIGN KEY(DEPT_CODE) REFERENCES DEPARTMENT; -- 생략하면 부모테이블의 PK로 감
+
+-- EMPLOYEE 테이블에 JOB_CODE에 외래키 제약조건 추가(JOB 테이블 참조)
+ALTER TABLE EMPLOYEE ADD FOREIGN KEY(JOB_CODE) REFERENCES JOB;
+-- EMPLOYEE 테이블 SAL_LEVEL에 외래키 제약조건(SAL_GRADE 테이블 참조)
+ALTER TABLE EMPLOYEE ADD FOREIGN KEY(SAL_LEVEL) REFERENCES SAL_GRADE; 
+-- DEPARTMENT 테이블에 LOCATION_ID에 외래키 제약조건(LOCATION 테이블 참조)
+ALTER TABLE EMPLOYEE ADD FOREIGN KEY(LOCATION_ID) REFERENCES LOCATION; --REFERENCES 참고하게 하겠다.
+
+
+
+
+
+
+
